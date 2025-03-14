5a386f07e82946df45b9c0a78d42b642ce19d5d1
gaurao-git <gurnulegaurao@gmail.com>
2025-03-14 20:58:58 +0530

    initial commit

---
+++ src/test/java/com/example/banking/config/server/BankingConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.banking.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
